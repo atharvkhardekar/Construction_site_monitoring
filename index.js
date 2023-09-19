48dd79bd344eebbe9404538489483e7e90061353
atharvkhardekar <142173608+atharvkhardekar@users.noreply.github.com>
2023-09-19 22:43:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+document.getElementById('logout').addEventListener('click', function () {
+    window.location.href = 'http://127.0.0.1:5501/login.html';
+});
+
+
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    loadSafetyData();
+    loadEfficiencyData();
+    loadMachineData();
+});
+
+function loadSafetyData() {
+   
+}
+
+function loadEfficiencyData() {
+   
+}
+
+function loadMachineData() {
+
+}
